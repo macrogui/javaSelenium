4600d2d421b76ee5ce9d3a3046500ac7bd3cb110
cytern <2477185748@qq.com>
2020-12-22 15:46:17 +0800

    addNew

---
+++ src/main/java/dam/jsoup/updatereport/updatreport/UpdatreportApplication.java
@@ -0,0 +1,13 @@
+package dam.jsoup.updatereport.updatreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdatreportApplication.class, args);
+    }
+
+}
